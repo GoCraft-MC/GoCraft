a66ece7eb1d1bf956a00f51b63cb378f987cab52
MaathDEV23 <174096315+MaathDEV23@users.noreply.github.com>
2026-08-31 19:28:16 +0200

    runtime: track isolated Go plugin processes

--- runtime/goplugin/instance.go
+++ runtime/goplugin/instance.go
@@ -1,10 +1,20 @@
 package goplugin
 
-import "GoCraft/core/plugin"
+import (
+	"sync"
+
+	"GoCraft/core/plugin"
+	"GoCraft/runtime/ipc"
+)
 
 // Instance is one native plugin process.
 type Instance struct {
-	manifest plugin.Manifest
+	runtime    *Runtime
+	manifest   plugin.Manifest
+	supervisor *ipc.Supervisor
+	cleanup    func()
+	unloadOnce sync.Once
+	unloadErr  error
 }
 
 func (i *Instance) Manifest() plugin.Manifest { return i.manifest }
