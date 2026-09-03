84b31916b2b3f9fed4d6bc63b3c16dc424158987
Elias <elias.saire@ynov.com>
2026-09-03 08:28:47 +0200

    Test splash potion player impacts

---
+++ server/splash_potion_test.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"testing"
+
+	corentity "GoCraft/core/entity"
+	"GoCraft/core/game"
+	"GoCraft/core/player"
+	"GoCraft/core/spatial"
+	"GoCraft/java/session"
+)
+
+func TestSplashPotionImpactAppliesDistanceScaledEffect(t *testing.T) {
+	g := game.New()
+	near := player.New([16]byte{51}, "near", player.ClientEditionJava)
+	near.Position = spatial.Vec3{X: 0, Y: 64, Z: 0}
+	far := player.New([16]byte{52}, "far", player.ClientEditionBedrock)
+	far.Position = spatial.Vec3{X: 5, Y: 64, Z: 0}
+	if err := g.AddPlayer(near); err != nil {
+		t.Fatal(err)
+	}
+	if err := g.AddPlayer(far); err != nil {
+		t.Fatal(err)
+	}
+	stack := player.ItemStack{ItemID: "minecraft:splash_potion", Count: 1}
+	if err := stack.SetComponent("potion_contents", map[string]string{"potion": "minecraft:poison"}); err != nil {
+		t.Fatal(err)
+	}
+	s := &Server{game: g, sessions: session.NewManager(), simulationDimension: dimensionOverworld}
+	s.resolveProjectileImpact(&corentity.Entity{Type: corentity.TypePotion, ProjectileItem: stack}, spatial.Vec3{Y: 64.9})
+
+	effect, ok := near.StatusEffect("poison")
+	if !ok || effect.Duration != 900 {
+		t.Fatalf("near poison = %+v, ok=%v", effect, ok)
+	}
+	if _, ok := far.StatusEffect("poison"); ok {
+		t.Fatal("out-of-range player received splash effect")
+	}
+}
+
+func TestSplashPotionImpactAppliesInstantHealing(t *testing.T) {
+	g := game.New()
+	p := player.New([16]byte{53}, "hurt", player.ClientEditionBedrock)
+	p.Health = 10
+	p.Position = spatial.Vec3{X: 2, Y: 64, Z: 2}
+	if err := g.AddPlayer(p); err != nil {
+		t.Fatal(err)
+	}
+	stack := player.ItemStack{ItemID: "minecraft:splash_potion", Count: 1}
+	if err := stack.SetComponent("potion_contents", map[string]string{"potion": "minecraft:healing"}); err != nil {
+		t.Fatal(err)
+	}
+	s := &Server{game: g, sessions: session.NewManager(), simulationDimension: dimensionOverworld}
+	s.applySplashPotion(stack, spatial.Vec3{X: 2, Y: 64.9, Z: 2})
+	if health, _, _, _ := p.HealthSnapshot(); health != 14 {
+		t.Fatalf("health after splash potion = %.1f, want 14", health)
+	}
+}
